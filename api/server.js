771a6f7efb064d3979ee106e910c3c556a5e1ed0
temitopeohassan <temitope.hassan.2@gmail.com>
2023-09-24 14:31:11 +0100

    API updated

--- api/server.js
+++ api/server.js
@@ -24,7 +24,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://gigworker.vercel.app/", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
